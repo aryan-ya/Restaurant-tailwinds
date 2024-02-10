9d2e13fd41d2b83365344d54241370f25fbfcc59
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-02-10 13:48:42 +0530

    hero

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
-import BiryaniImg1 from "../../assets/biryani3.png";
-import BiryaniImg2 from "../../assets/biryani5.png";
-import BiryaniImg3 from "../../assets/biryani2.png";
+import Food1 from "../../assets/biryani3.png";
+import Food2 from "../../assets/biryani5.png";
+import Food3 from "../../assets/biryani2.png";
 import Vector from "../../assets/vector3.png";
 
+const ImageList = [
+  {
+     id: 1,
+     image : Food1,
+
+  },
+
+  {
+    id: 2,
+    image : Food2,
+
+ },
+ {
+  id: 3,
+  image : Food3,
+
+},
+];
+
+const bgImage = {
+  backgroundImage : `url(${Vector})`,
+  backgroundPosition : "center",
+  backgroundSize :"cover",
+  backgroundRepeat : "no-repeat",
+  width : "100%",
+  height: "100%",
+}
+
+
+
 const Hero = () => {
+  const [imageId , setImageId] = React.useState(Food1);
   return (
-    <div></div>
+    <div style={bgImage} className="min-h-[550px] sm:min-h-[600px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200"
+    ></div>
   )
 }
 
