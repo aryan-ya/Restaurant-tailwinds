448bc497c912efee6b507e897d424c9916a28a18
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-02-12 10:33:46 +0530

    testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import Service from './Component/Service/Service'
 import Banner from './Component/Banner/Banner'
 import AppStore from './Component/AppStore/AppStore'
 import Testimonial from './Component/TestiMonial/Testimonial'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div className="">
       <Navbar />
--- src/Component/TestiMonial/Testimonial.jsx
+++ src/Component/TestiMonial/Testimonial.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const testimonialData = [
   {
@@ -11,51 +11,65 @@ const testimonialData = [
     img: "https://picsum.photos/101/101",
   },
   {
-    id: 1,
+    id: 2,
     name: "John Doe",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/102/102",
   },
   {
-    id: 1,
+    id: 3,
     name: "Smith",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/103/103",
   },
 ];
-const Testimonial = () => {
-    var settings = {
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      cssEase: "linear",
-      pauseOnHover: true,
-      pauseOnFocus: true,
-    };
-  
 
+const Testimonial = () => {
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+  };
 
   return (
-    <div data-aos="fade-up" data-aos-duration="300" className="py-10">
+    <div className="py-10 bg-gray-100">
       <div className="container">
-        <div className="text-center mb-20 max-w-[400px] mx-auto" >
-           <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"> What our customers say</p> 
-           <h1 className="text-3xl font-bold">Testimonial</h1>
-           <p className="text-xs text-gray-400">  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Perspiciatis delectus architecto error nesciunt,</p> 
-          
+        <div className="text-center mb-10">
+          <p className="text-sm text-primary">What our customers say</p>
+          <h1 className="text-3xl font-bold">Testimonials</h1>
+          <p className="text-xs text-gray-400">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perspiciatis delectus architecto error nesciunt,
+          </p>
         </div>
-        <div  data-aos="zoom-in"
-            data-aos-duration="300" className="grid grid-cols-1 max-w-[600px] mx-auto gap-6"></div>
+        <div className="max-w-lg mx-auto">
+          <Slider {...settings}>
+            {testimonialData.map((data) => (
+              <div key={data.id} className="mb-8">
+                <div className="flex flex-col items-center gap-4 text-center shadow-lg p-4 rounded-xl bg-white">
+                  <img
+                    className="rounded-full w-20 h-20 mx-auto"
+                    src={data.img}
+                    alt=""
+                  />
+                  <p className="text-gray-500 text-sm">{data.text}</p>
+                  <h1 className="text-xl font-bold">{data.name}</h1>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
-        
     </div>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
